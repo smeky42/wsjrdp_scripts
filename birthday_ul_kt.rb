7e3edc0c20c83029a506aa799c090483e014972f
Peter Neubauer <development@smeky.de>
2022-09-15 21:59:42 +0200

    changed date

--- birthday_ul_kt.rb
+++ birthday_ul_kt.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-#  Copyright (c) 2012-2021, German Contingent for the Worldscoutjamboree 2023. This file is part of
+#  Copyright (c) 2012-2022, German Contingent for the Worldscoutjamboree 2023. This file is part of
 #  hitobito_wsjrdp_2023 and licensed under the Affero General Public License version 3
 #  or later. See the COPYING file at the top-level directory or at
 #  https://github.com/hitobito/hitobito_wsjrdp_2023 and https://github.com/hitobito/wsjrdp_scripts.
