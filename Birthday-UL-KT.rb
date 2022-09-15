0fd7edf64ba36e48b4c0e5bcb2ffde57c75cd744
Peter Neubauer <development@smeky.de>
2022-09-15 21:54:23 +0200

    refacored birthday

--- Birthday-UL-KT.rb
+++ Birthday-UL-KT.rb
@@ -7,12 +7,12 @@ require 'yaml'
 config = YAML.load_file('./config.yml')
 
 puts 'Starting to process'
-days = ["07","08","09"]
-month = ["10"]
+days = %w[07 08 09]
+month = ['10']
 
 puts 'Conntect to SQL'
-client = Mysql2::Client.new(host: 'anmeldung.worldscoutjamboree.de', username: config["username"],
-                            password: config["password"], database: config["database"], port: config["port"])
+client = Mysql2::Client.new(host: 'anmeldung.worldscoutjamboree.de', username: config['username'],
+                            password: config['password'], database: config['database'], port: config['port'])
 
 puts "\n Starte generierung für Unit Leitungen und KT"
 query = 'select p.id, p.first_name, p.last_name, p.birthday
@@ -22,14 +22,14 @@ query = 'select p.id, p.first_name, p.last_name, p.birthday
           and (status = "bestätigt durch KT" or status = "bestätigt durch Leitung")
           or id = 1078;' # 1078 Mio
 people = client.query(query)
-puts 'mit ' + people.count.to_s + ' Mitgliedern'
+puts "mit #{people.count} Mitgliedern"
 
 people.each do |person|
-  birthday = person["birthday"].to_s.split("-")
+  birthday = person['birthday'].to_s.split('-')
   birthday_day = birthday[2]
   birthday_month = birthday[1]
 
-  if ( days.include? birthday_day ) && ( month.include? birthday_month )
-    puts person["id"].to_s + ": " + person["first_name"] + " " + person["last_name"] + " " + person["birthday"].to_s
-  end 
-end 
+  if (days.include? birthday_day) && (month.include? birthday_month)
+    puts "#{person['id']}: #{person['first_name']} #{person['last_name']} - #{person['birthday']}"
+  end
+end
