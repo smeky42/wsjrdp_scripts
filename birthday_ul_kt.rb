6143c28b9b7df84082000dec5ecb59449efff988
Peter Neubauer <development@smeky.de>
2022-09-15 21:58:19 +0200

    licence

---
+++ birthday_ul_kt.rb
@@ -0,0 +1,42 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2012-2021, German Contingent for the Worldscoutjamboree 2023. This file is part of
+#  hitobito_wsjrdp_2023 and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_wsjrdp_2023 and https://github.com/hitobito/wsjrdp_scripts.
+
+require 'mysql2'
+require 'csv'
+require 'openssl'
+require 'net/smtp'
+require 'yaml'
+
+config = YAML.load_file('./config.yml')
+
+puts 'Starting to process'
+days = %w[07 08 09]
+month = ['10']
+
+puts 'Conntect to SQL'
+client = Mysql2::Client.new(host: 'anmeldung.worldscoutjamboree.de', username: config['username'],
+                            password: config['password'], database: config['database'], port: config['port'])
+
+puts "\n Starte generierung für Unit Leitungen und KT"
+query = 'select p.id, p.first_name, p.last_name, p.birthday
+          from people p
+          where id > 1
+          and (role_wish="Unit Leitung" or role_wish="Kontingentsteam")
+          and (status = "bestätigt durch KT" or status = "bestätigt durch Leitung")
+          or id = 1078;' # 1078 Mio
+people = client.query(query)
+puts "mit #{people.count} Mitgliedern"
+
+people.each do |person|
+  birthday = person['birthday'].to_s.split('-')
+  birthday_day = birthday[2]
+  birthday_month = birthday[1]
+
+  if (days.include? birthday_day) && (month.include? birthday_month)
+    puts "#{person['id']}: #{person['first_name']} #{person['last_name']} - #{person['birthday']}"
+  end
+end
