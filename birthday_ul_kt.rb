6b2eddaaee0d4de2d57d462ddd1b679bf3467545
Peter Neubauer <development@smeky.de>
2023-03-17 13:39:46 +0100

    updated birthday calculation

--- birthday_ul_kt.rb
+++ birthday_ul_kt.rb
@@ -14,29 +14,37 @@ require 'yaml'
 config = YAML.load_file('./config.yml')
 
 puts 'Starting to process'
-days = %w[07 08 09]
-month = ['10']
+start_date = Date.new(2023, 7, 28)
+end_date = Date.new(2023, 8, 8)
+valid_days = []
+
+(start_date..end_date).each do |day|
+  # puts "#{day.day}.#{day.month}"
+  valid_days.append("#{day.day}.#{day.month}")
+end
 
 puts 'Conntect to SQL'
 client = Mysql2::Client.new(host: 'anmeldung.worldscoutjamboree.de', username: config['username'],
                             password: config['password'], database: config['database'], port: config['port'])
 
 puts "\n Starte generierung für Unit Leitungen und KT"
-query = 'select p.id, p.first_name, p.last_name, p.birthday
+query = 'select p.id, p.first_name, p.last_name, p.birthday, p.role_wish
           from people p
           where id > 1
-          and (role_wish="Unit Leitung" or role_wish="Kontingentsteam")
-          and (status = "bestätigt durch KT" or status = "bestätigt durch Leitung")
+          and (role_wish="IST" or role_wish="Kontingentsteam")
+          and (status = "bestätigt durch KT" or status = "bestätigt durch Leitung" or status = "vollständig")
           or id = 1078;' # 1078 Mio
 people = client.query(query)
 puts "mit #{people.count} Mitgliedern"
 
-people.each do |person|
-  birthday = person['birthday'].to_s.split('-')
-  birthday_day = birthday[2]
-  birthday_month = birthday[1]
+# puts valid_days
 
-  if (days.include? birthday_day) && (month.include? birthday_month)
-    puts "#{person['id']}: #{person['first_name']} #{person['last_name']} - #{person['birthday']}"
+people.each do |person|
+  birthday = person['birthday']
+
+  # puts "#{person['birthday']} #{birthday.day}.#{birthday.month}"
+
+  if valid_days.include? "#{birthday.day}.#{birthday.month}"
+    puts "#{person['role_wish']} #{person['id']}: #{person['first_name']} #{person['last_name']} - #{person['birthday']}"
   end
 end
