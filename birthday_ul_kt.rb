de84fe8dc794f8853b3c6fe464ccd32a646ca29c
Peter Neubauer <development@smeky.de>
2023-03-25 10:24:58 +0100

    create birthdays

--- birthday_ul_kt.rb
+++ birthday_ul_kt.rb
@@ -11,17 +11,29 @@ require 'openssl'
 require 'net/smtp'
 require 'yaml'
 
+def create_date_array(start_date, end_date)
+  valid_days = []
+
+  (start_date..end_date).each do |day|
+    valid_days.append("#{day.day}.#{day.month}")
+  end
+
+  valid_days
+end
+
+def print_days(people, valid_days)
+  people.each do |person|
+    birthday = person['birthday']
+
+    if valid_days.include? "#{birthday.day}.#{birthday.month}"
+      puts "#{person['role_wish']} #{person['id']}: #{person['first_name']} #{person['last_name']} - #{person['birthday']}"
+    end
+  end
+end
+
 config = YAML.load_file('./config.yml')
 
 puts 'Starting to process'
-start_date = Date.new(2023, 7, 28)
-end_date = Date.new(2023, 8, 8)
-valid_days = []
-
-(start_date..end_date).each do |day|
-  # puts "#{day.day}.#{day.month}"
-  valid_days.append("#{day.day}.#{day.month}")
-end
 
 puts 'Conntect to SQL'
 client = Mysql2::Client.new(host: 'anmeldung.worldscoutjamboree.de', username: config['username'],
@@ -31,20 +43,26 @@ puts "\n Starte generierung für Unit Leitungen und KT"
 query = 'select p.id, p.first_name, p.last_name, p.birthday, p.role_wish
           from people p
           where id > 1
-          and (role_wish="IST" or role_wish="Kontingentsteam")
+          and (role_wish="IST" or role_wish="Kontingentsteam" or role_wish="Unit Leitung")
           and (status = "bestätigt durch KT" or status = "bestätigt durch Leitung" or status = "vollständig")
           or id = 1078;' # 1078 Mio
 people = client.query(query)
 puts "mit #{people.count} Mitgliedern"
 
-# puts valid_days
+puts 'KT Wolfsburg 14. - 16.04.'
+start_date = Date.new(2023, 4, 14)
+end_date = Date.new(2023, 4, 16)
+valid_days_kt = create_date_array(start_date, end_date)
+print_days(people, valid_days_kt)
 
-people.each do |person|
-  birthday = person['birthday']
+puts 'KoLa 17. - 21.05'
+start_date = Date.new(2023, 5, 17)
+end_date = Date.new(2023, 5, 21)
+valid_days_kola = create_date_array(start_date, end_date)
+print_days(people, valid_days_kola)
 
-  # puts "#{person['birthday']} #{birthday.day}.#{birthday.month}"
-
-  if valid_days.include? "#{birthday.day}.#{birthday.month}"
-    puts "#{person['role_wish']} #{person['id']}: #{person['first_name']} #{person['last_name']} - #{person['birthday']}"
-  end
-end
+puts 'AKT Jamboree 29.07 - 08.12'
+start_date = Date.new(2023, 7, 29)
+end_date = Date.new(2023, 8, 12)
+valid_days_jamb = create_date_array(start_date, end_date)
+print_days(people, valid_days_jamb)
