953299692199ac213d1e48b945addb93f64aef9c
Peter Neubauer <development@smeky.de>
2023-04-19 20:41:04 +0200

    update script korea excel id

---
+++ upload_korea/read_id.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+import os
+import sys
+import typing
+import warnings
+from datetime import date
+
+import yaml
+from mysql.connector import connection
+from openpyxl import load_workbook
+
+import json
+
+
+def main():
+    with open("../config.yml", "r") as yamlfile:
+        config = yaml.load(yamlfile, Loader=yaml.FullLoader)
+        print("Read successful")
+
+    # today = str(date.today())
+    cnx = connection.MySQLConnection(
+        user=config["username"],
+        password=config["password"],
+        host="anmeldung.worldscoutjamboree.de",
+        port=config["port"],
+        database=config["database"],
+    )
+
+    cursor = cnx.cursor(dictionary=True)
+    
+    # load excel file
+    workbook = load_workbook(filename="2023-04-20--Download-Korea.xlsx")
+    worksheet = workbook.active
+    k_id_column = 3
+    url_column = 27
+ 
+    print(worksheet.cell(column=28, row=4).value)
+
+    for row in worksheet.iter_rows():
+        korea_id = row[k_id_column].value
+        id = str(row[url_column].value).split("/")[-1].replace(".html","")
+        cursor.execute(f"update people set korea_id = '#{korea_id}' where id='#{id}'")
+        print(f"Updated id=#{id} with korea_id=#{korea_id}  ")
+    
+
+if __name__ == "__main__":
+    sys.exit(main())
