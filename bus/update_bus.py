655c31c9b096b22e01172d6b3ec95f7b55269335
Peter Neubauer <development@smeky.de>
2023-05-01 08:37:11 +0200

    busliste ohne event

--- bus/update_bus.py
+++ bus/update_bus.py
@@ -4,7 +4,6 @@ from datetime import date
 
 import yaml
 from mysql.connector import connection
-#from openpyxl import load_workbook
 import pandas as pd
 
 def main():
@@ -21,25 +20,45 @@ def main():
       database=config["database"],
   )
 
-  db_people = pd.read_sql_query('''SELECT id, first_name, last_name FROM people LIMIT 10''', cnx)
+  db_people = pd.read_sql_query("""SELECT id, primary_group_id, first_name, last_name, kola_participation, kola_bus, kola_reason FROM people 
+                                    WHERE status not in ('abgemeldet', 'Abmeldung Vermerkt', 'in Überprüfung durch KT', '')
+                                    and role_wish <> '' """, cnx)
   df_people = pd.DataFrame(db_people)
-  print(df_people.columns)
-  print(df_people.head(10))
+
+  db_groups = pd.read_sql_query('''SELECT id, name FROM groups''', cnx)
+  df_groups = pd.DataFrame(db_groups)
+  df_groups = df_groups.rename(columns={'id': 'group_id','name': 'group_name'})
+
+  df = pd.merge(df_people, df_groups, left_on='primary_group_id', right_on='group_id',how='left')
 
   df_bus = pd.read_excel('2023-04-29--Busliste.xlsx')
   df_bus = df_bus.rename(columns={'TN ID': 'tn_id', 'Adresse': 'adresse', 'PLZ': 'plz', 'Ort': 'ort', 'Bus Route': 'bus_route', 'Busnummer': 'busnummer',
        'Hinfahrt Abfahrtsort': 'hinfahrt_abfahrtsort', 'Definitiver Abfahrtsort': 'definitiver_abfahrtsort', 'Abfahrtszeit': 'abfahrtszeit',
        'Ankunftszeit Immenhausen': 'ankunftszeit_immenhausen', 'Abfahrt Immenhausen am Sonntag ': 'abfahrt_immenhausen_am_sonntag',
-       'Ankunftsort Rückfahrt': 'ankunftsort_rückfahrt', 'Ankunftszeit Rückfahrt': 'ankunftszeit_rückfahrt',
-       'Zusätzliches Material Gepäck': 'zusätzliches_material_gepäck', 'Hinweise': 'hinweise'})
-  print(df_bus.columns)
+       'Ankunftsort Rückfahrt': 'ankunftsort_rueckfahrt', 'Ankunftszeit Rückfahrt': 'ankunftszeit_rueckfahrt',
+       'Zusätzliches Material Gepäck': 'zusätzliches_material_gepaeck', 'Hinweise': 'hinweise'})
 
-  df = pd.merge(df_people, df_bus, left_on='id', right_on='tn_id',how='left')
-  print(df.columns)
-  # print(df_bus['TN ID'].to_string(index=False))
+  df = pd.merge(df, df_bus, left_on='id', right_on='tn_id',how='left')
+  #print(df.columns)
 
+  cursor = cnx.cursor()
+  rows_affected = 0
   for row in df.itertuples():
-     print(row.hinfahrt_abfahrtsort)  
+    #print(row)
+    if pd.isna(row.abfahrtszeit):
+       bus_travel = "Für dich ist keine Busanreise zum KoLa geplant."
+    else:
+      bus_travel = f"""Hinfahrt: {row.abfahrtszeit} {row.definitiver_abfahrtsort} -> {row.ankunftszeit_immenhausen} Immenhausen \n 
+      Rückfahrt: {row.abfahrt_immenhausen_am_sonntag} Immenhausen -> {row.ankunftszeit_rueckfahrt} {row.ankunftsort_rueckfahrt}"""
+    
+    update = f"update people set bus_travel='{bus_travel}' where id='{row.id}'"
+    #print(update)
+    cursor.execute(update)
+    cnx.commit()
+    rows_affected += cursor.rowcount
+  print (rows_affected, "record(s) affected")
+
+  df.to_excel(f"{str(date.today())}--Bus-Immenhausen.xlsx", sheet_name="Busreise", index=False)
 
 if __name__ == "__main__":
     sys.exit(main())
