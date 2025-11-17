a2c3ca3c62b60a62557a3baab5e7480ad5763aad
David Fritzsche <dfritzsche@daffi.net>
2025-11-17 15:29:55 +0100

    Update tools/stats_roles_status.py to deal with waiting list

--- tools/stats_roles_status.py
+++ tools/stats_roles_status.py
@@ -4,7 +4,6 @@
 from __future__ import annotations
 
 import logging
-import math
 import sys
 import textwrap
 
@@ -17,11 +16,19 @@ _LOGGER = logging.getLogger(__name__)
 
 
 STATS_ROLES_STATUS_SQL_STMT = """
-WITH group_names (id, name) AS (VALUES (1, 'CMT'),(2, 'UL'),(3, 'YP'),(4, 'IST'))
+WITH group_names (id, name) AS (VALUES
+ (1, 'CMT'),
+ (2, 'UL'),
+ (3, 'YP'),
+ (4, 'IST'),
+ (5, 'UL Warteliste'),
+ (6, 'YP Warteliste'),
+ (7, 'IST Warteliste')
+)
 
 SELECT group_names.name AS role, status, COUNT(people.id) AS num_people FROM people
 JOIN group_names ON people.primary_group_id = group_names.id
-WHERE primary_group_id IN (1, 2, 3, 4)
+WHERE primary_group_id IN (1, 2, 3, 4, 5, 6, 7)
 AND status NOT IN ('deregistration_noted', 'deregistered')
 
 GROUP BY group_names.name, people.primary_group_id, status
@@ -30,7 +37,9 @@ ORDER BY
   array_position(array['registered','printed','upload','in_review','reviewed','confirmed','deregistration_noted', 'deregistered'], status)
 """
 
-ROLES = ["CMT", "UL", "YP", "IST"]
+REG_ROLES = ["CMT", "UL", "YP", "IST"]
+WAIT_ROLES = ["UL Warteliste", "YP Warteliste", "IST Warteliste"]
+ALL_ROLES = REG_ROLES + WAIT_ROLES
 
 COLUMNS = [
     "registered",
@@ -43,13 +52,7 @@ COLUMNS = [
 
 
 def main(argv=None):
-    start_time = None
-    # start_time = datetime.datetime(2025, 8, 15, 10, 30, 27).astimezone()
-    ctx = wsjrdp2027.WsjRdpContext(
-        setup_logging=True,
-        start_time=start_time,
-        out_dir="data",
-    )
+    ctx = wsjrdp2027.WsjRdpContext(setup_logging=True, out_dir="data")
     out_base = ctx.make_out_path("stats_roles_status_{{ filename_suffix }}")
     xlsx_filename = out_base.with_suffix(".xlsx")
 
@@ -59,7 +62,7 @@ def main(argv=None):
         rows = cur.fetchall()
         cur.close()
         base_df = pd.DataFrame(rows)
-    _LOGGER.info(
+    _LOGGER.debug(
         "Resulting base pandas DataFrame:\n%s", textwrap.indent(str(base_df), "  ")
     )
 
@@ -71,18 +74,29 @@ def main(argv=None):
             *(f"min_{col}" for col in COLUMNS if col not in NO_MIN_COLS),
         ]
     )
-    for role in ROLES:
-        for col in df.columns[1:]:
+    for role in REG_ROLES:
+        for col in df.columns[0:]:
+            df.at[role, col] = 0
+    df.loc[len(df)] = [""] * len(df.columns)
+    for role in WAIT_ROLES:
+        for col in df.columns[0:]:
             df.at[role, col] = 0
     for _, row in base_df.iterrows():
         df.at[row["role"], row["status"]] = int(row["num_people"])
     for idx, row in df.iterrows():
+        if not isinstance(idx, str):
+            continue
         for j, col in enumerate(COLUMNS):
             if col not in NO_MIN_COLS:
                 sum_range = slice(j, len(COLUMNS))
-                df.at[idx, f"min_{col}"] = sum(row[sum_range])
+                df.at[idx, f"min_{col}"] = sum(
+                    x for x in row[sum_range] if isinstance(x, (float, int))
+                )
 
-    df.loc["Alle"] = sum(df.loc[role] for role in ROLES)
+    df.loc[len(df)] = [""] * len(df.columns)
+    df.loc["Alle"] = sum(df.loc[role] for role in REG_ROLES)
+    df.loc["Alle (inkl. Warteliste)"] = sum(df.loc[role] for role in ALL_ROLES)
+    df.loc[len(df)] = [""] * len(df.columns)
     yp_row = df.loc["YP"]
     ul_row = df.loc["UL"]
     df.loc["YP+UL"] = yp_row + ul_row
@@ -95,8 +109,18 @@ def main(argv=None):
     _LOGGER.info("Resulting base pandas DataFrame:\n%s", textwrap.indent(str(df), "  "))
 
     export_df = df.copy()
-    export_df.insert(0, "role", list(df.index))
-    wsjrdp2027.write_dataframe_to_xlsx(export_df, xlsx_filename)
+    export_df.insert(len(COLUMNS), "-", "")
+    export_df.insert(
+        0, "role", [(idx if isinstance(idx, str) else "") for idx in df.index]
+    )
+    header = [("" if col.startswith("-") else col) for col in export_df.columns]
+    wsjrdp2027.write_dataframe_to_xlsx(
+        export_df,
+        xlsx_filename,
+        add_autofilter=False,
+        float_format="%.2f",
+        header=header,
+    )
 
 
 if __name__ == "__main__":
