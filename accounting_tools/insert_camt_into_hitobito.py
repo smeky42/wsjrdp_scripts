cb4d21237179afb53c256782df8c83301b77db86
David Fritzsche <dfritzsche@daffi.net>
2026-02-28 00:36:54 +0100

    Update CAMT import to support WSJ27 Pax-Bank money market account

--- accounting_tools/insert_camt_into_hitobito.py
+++ accounting_tools/insert_camt_into_hitobito.py
@@ -20,7 +20,34 @@ def _load_fin_accounts(conn, /) -> dict[str, int]:
     return {row["account_identification"]: row["id"] for row in fin_accounts}
 
 
+def get_fin_account_id(
+    ctx,
+    conn,
+    account_identification: str,
+    account_identification2fin_account_id: dict[str, int],
+) -> int:
+    try:
+        return account_identification2fin_account_id[account_identification]
+    except KeyError:
+        _LOGGER.warning(f"Account {account_identification} not in DB")
+
+        acc = wsjrdp2027.bank_accounts.WSJ27_ACCOUNT_IDENTIFICATION_TO_BANK_ACCOUNT_DICT.get(
+            account_identification
+        )
+        if acc:
+            _LOGGER.info(f"Need to insert new account into db:\n{acc}")
+            ctx.require_approval_to_run_in_prod("Insert new account into db")
+
+            acc_id = wsjrdp2027.pg_insert_fin_account(conn, **acc.asdict())
+            account_identification2fin_account_id[account_identification] = acc_id
+            return acc_id
+        else:
+            _LOGGER.error("  Account unknown -> exit")
+            raise RuntimeError(f"Unknown account {account_identification!r}")
+
+
 def _read_camt_and_write_to_db(
+    ctx,
     conn,
     /,
     path: _pathlib.Path | str,
@@ -50,9 +77,12 @@ def _read_camt_and_write_to_db(
             for k, v in refs.items():
                 _LOGGER.info(f"    {k}: {v}")
         # TODO: Handle new accounts appearing in the CAMT file
-        fin_account_id = account_identification2fin_account_id[
-            tx.account_identification
-        ]
+        fin_account_id = get_fin_account_id(
+            ctx,
+            conn,
+            tx.account_identification,
+            account_identification2fin_account_id=account_identification2fin_account_id,
+        )
         if tx_row := tx_unique_db_key2row.get(tx.unique_db_key):
             errors = []
             for key in ["amount_cents", "amount_currency", "value_date"]:
@@ -101,6 +131,7 @@ def main(argv=None):
             _LOGGER.info("")
             _LOGGER.info("CAMT file: %s", camt_file)
             _read_camt_and_write_to_db(
+                ctx,
                 conn,
                 camt_file,
                 account_identification2fin_account_id=account_identification2fin_account_id,
